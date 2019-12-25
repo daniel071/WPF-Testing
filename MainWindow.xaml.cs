9c3409ae9d8dccfb608c875208bc4bf10e426169
Daniel Pavela <daniel.pavela@gmail.com>
2019-12-25 17:11:08 +1100

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Testing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Number1_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            Console.WriteLine(e);
+        }
+
+        private void Number2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Calculate__Click(object sender, RoutedEventArgs e)
+        {
+            double FirstNumber = Convert.ToDouble(Number1.Text);
+            double SecondNumber = Convert.ToDouble(Number2.Text);
+            double Answer = FirstNumber + SecondNumber;
+            theAnswer.Text = "The answer is: \n" + Answer;
+        }
+
+    }
+}
